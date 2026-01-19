34831f488a73faece1e836040d9cac0c98bbc940
Md-Faizan-Rasheed <s1391792@gmail.com>
2026-01-19 18:11:29 +0530

    change the varialbe name

--- src/components/OrganisationComponents/service/ai/openaiClient.js
+++ src/components/OrganisationComponents/service/ai/openaiClient.js
@@ -1,14 +1,13 @@
 import OpenAI from "openai";
 
 
-const OPENAI_API_KEY_FRONTEND = import.meta.env.VITE_OPENAI_API_KEY_FRONTEND ;
+const VITE_OPENAI_API_KEY_FRONTEND = import.meta.env.VITE_OPENAI_API_KEY_FRONTEND ;
 
-console.log("🔑 OpenAI API Key loaded:", OPENAI_API_KEY_FRONTEND ? "✅ Loaded" : "❌ Missing");
+console.log("🔑 OpenAI API Key loaded:", VITE_OPENAI_API_KEY_FRONTEND
+ ? "✅ Loaded" : "❌ Missing");
 
 const openai = new OpenAI({
-//   apiKey: import.meta.env.OPENAI_API_KEY,
-apiKey: OPENAI_API_KEY_FRONTEND,
-// apiKey:OPENAI_API_KEY,
+apiKey: VITE_OPENAI_API_KEY_FRONTEND,
   dangerouslyAllowBrowser: true, // REQUIRED for frontend
 });
 
