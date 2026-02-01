faf0e0bf3227f0ac5e7a9c9a0fcfe052c4cf4daa
Md-Faizan-Rasheed <s1391792@gmail.com>
2026-02-01 12:01:13 +0530

    change key name

--- src/components/OrganisationComponents/service/ai/openaiClient.js
+++ src/components/OrganisationComponents/service/ai/openaiClient.js
@@ -1,25 +1,7 @@
-// import OpenAI from "openai";
-
-
-// console.log(import.meta.env);
-
-// const VITE_OPENAI_API_KEY_FRONTEND = import.meta.env.VITE_OPENAI_API_KEY_FRONTEND ;
-// console.log("🚀 VITE_OPENAI_API_KEY_FRONTEND:", VITE_OPENAI_API_KEY_FRONTEND) ;
-// console.log("🔑 OpenAI API Key loaded:", import.meta.env.VITE_OPENAI_API_KEY_FRONTEND
-//  ? "✅ Loaded" : "❌ Missing");
-
-// const openai = new OpenAI({
-// apiKey: VITE_OPENAI_API_KEY_FRONTEND,
-//   dangerouslyAllowBrowser: true, // REQUIRED for frontend
-// });
-
-// export default openai;
-
-
 
 import OpenAI from "openai";
 
-const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY_FRONTEND;
 
 console.log("API loaded:", apiKey ? "YES" : "NO");
 
